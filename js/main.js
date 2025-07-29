612783ce4945372c5eb59ded9283940ec24c6ef5
Мария Курильчик <139053449+MaremianaComp@users.noreply.github.com>
2025-07-29 19:42:58 +0200

    Adds validation

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@
 // import { picturesNode } from './render-cards.js';
 import { container } from './thumbnails.js';
 import { openBigPicture } from './render-photo.js';
+import { initUploadModal } from './upload-photo-form.js';
+
+// document.addEventListener('DOMContentLoaded', () => {
+initUploadModal();
+// });
 
 container.addEventListener('click', (evt) => {
   const currentPictureNode = evt.target.closest('.picture');
