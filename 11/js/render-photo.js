f544b4270ddcf39664d5bc7bc8e687137e5b9abc
Keks <keks@htmlacademy.ru>
2025-07-30 21:17:01 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/render-photo.js
@@ -0,0 +1,74 @@
+import { photos } from './data-generation.js';
+import { initComments, clearComments } from './render-comments.js';
+import { isEscapeKey } from './utils/escape.js';
+
+const bigPictureNode = document.querySelector('.big-picture');
+const bigPictureImgNode = bigPictureNode.querySelector('.big-picture__img img');
+const likesCountNode = bigPictureNode.querySelector('.likes-count');
+const commentsCaptionNode = bigPictureNode.querySelector('.social__caption');
+const bigPictureCancelNode = bigPictureNode.querySelector('.big-picture__cancel');
+
+// Обработчики закрытия модального окна
+const onBigPictureCancelClick = (evt) => {
+  evt.preventDefault();
+  closeBigPicture();
+};
+
+const onEscKeydown = (evt) => {
+  // if (evt.key === 'Escape') {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeBigPicture();
+  }
+};
+
+// const onOutsideClick = (evt) => {
+//   if (evt.target === bigPictureNode) {
+//     closeBigPicture();
+//   }
+// };
+
+// Функция закрытия модального окна
+const closeBigPicture = () => {
+  bigPictureNode.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+
+  // Удаляем все обработчики
+  bigPictureCancelNode.removeEventListener('click', onBigPictureCancelClick);
+  document.removeEventListener('keydown', onEscKeydown);
+  // bigPictureNode.removeEventListener('click', onOutsideClick);
+
+  // Очищаем комментарии
+  clearComments();
+};
+
+// Функция открытия модального окна
+const openBigPicture = (pictureId) => {
+  // Находим фото по ID
+  const currentPhoto = photos.find((photo) => photo.id === Number(pictureId));
+
+  // if (!currentPhoto) {
+  //   console.error(`Photo with ID ${pictureId} not found`);
+  //   return;
+  // }
+
+  // Заполняем данные фото
+  bigPictureImgNode.src = currentPhoto.url;
+  bigPictureImgNode.alt = currentPhoto.description;
+  likesCountNode.textContent = currentPhoto.likes;
+  commentsCaptionNode.textContent = currentPhoto.description;
+
+  // Инициализируем комментарии с порционной загрузкой
+  initComments(currentPhoto.comments);
+
+  // Показываем модальное окно
+  bigPictureNode.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+
+  // Добавляем обработчики
+  bigPictureCancelNode.addEventListener('click', onBigPictureCancelClick);
+  document.addEventListener('keydown', onEscKeydown);
+  // bigPictureNode.addEventListener('click', onOutsideClick);
+};
+
+export { openBigPicture };
---
+++ 11/js/utils/num-decline.js
@@ -0,0 +1,37 @@
+/**
+ * Функция для склонения слова в зависимости от числа
+ * @param {number} num - Число
+ * @param {string} nominative - Именительный падеж (1 хештег)
+ * @param {string} genitiveSingular - Родительный падеж единственного числа (2 хештега)
+ * @param {string} genitivePlural - Родительный падеж множественного числа (5 хештегов)
+ * @returns {string} - Правильная форма слова
+ */
+export const numDecline = (num, nominative, genitiveSingular, genitivePlural) => {
+  // Для чисел, оканчивающихся на 0 или от 5 до 20, используем множественное число
+  if (num % 10 === 0 || (num % 100 >= 5 && num % 100 <= 20)) {
+    return genitivePlural;
+  }
+
+  // Для чисел, оканчивающихся на 1 (кроме 11) - именительный падеж
+  if (num % 10 === 1 && num % 100 !== 11) {
+    return nominative;
+  }
+
+  // Для чисел 2-4 (кроме 12-14) - родительный падеж единственного числа
+  if (num % 10 >= 2 && num % 10 <= 4 && !(num % 100 >= 12 && num % 100 <= 14)) {
+    return genitiveSingular;
+  }
+
+  // Во всех остальных случаях - множественное число
+  return genitivePlural;
+};
+
+// export function numDecline(number, one, few, many) {
+//   if (number % 10 === 1 && number % 100 !== 11) {
+//     return one;
+//   }
+//   if ([2, 3, 4].includes(number % 10) && ![12, 13, 14].includes(number % 100)) {
+//     return few;
+//   }
+//   return many;
+// }
